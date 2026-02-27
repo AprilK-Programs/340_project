6458b058cefffdd365ecf2fa5410f85002f65d83
Matt Gomez <mtthw.gmez@gmail.com>
2026-02-27 09:06:04 -0800

    changed app.js to import the .env credentials so as to not hardcode our port number on the repository

--- app.js
+++ app.js
@@ -3,13 +3,15 @@
 // ########## SETUP
 
 // Express
+require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const PORT = 1335;
+let PORT = process.env.PORT;
+
 
 // Database
 const db = require('./database/db-connector');
